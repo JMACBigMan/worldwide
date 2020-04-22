eaeb05a98433806f03fab48a1bf6447f745a2abb
JMACBigMan <jemclaurin@gmail.com>
2020-04-22 15:55:47 -0400

    Initial commit

---
+++ app/src/main/java/jmac/application/worldwide/Activities/AddPostActivity.java
@@ -0,0 +1,164 @@
+package jmac.application.worldwide.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jmac.application.worldwide.R;
+
+public class AddPostActivity extends AppCompatActivity {
+    private ImageButton mPostImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitButton;
+    private StorageReference mStorage;
+    private DatabaseReference mPostDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ProgressDialog mProgress;
+    private Uri mImageUri;
+    private static final int GALLERY_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+        mProgress = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("MBlog");
+
+        mPostImage = (ImageButton) findViewById(R.id.imageButton);
+        mPostTitle = (EditText) findViewById(R.id.postTitleEt);
+        mPostDesc = (EditText)findViewById(R.id.descriptionEt);
+        mSubmitButton = (Button) findViewById(R.id.submitPost);
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+            }
+        });
+
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Posting to our database
+                startPosting();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+            mPostImage.setImageURI(mImageUri);
+
+
+        }
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Posting to blog...");
+        mProgress.show();
+
+        final String titleVal = mPostTitle.getText().toString().trim();
+        final String descVal = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)
+                && mImageUri != null) {
+            //start the uploading...
+            final StorageReference filepath = mStorage.child("MBlog_images").
+                    child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return filepath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+
+                    if (task.isSuccessful()) {
+                        Uri downloadurl = task.getResult();
+                        DatabaseReference newPost = mPostDatabase.push();
+
+                        Map<String, String> dataToSave = new HashMap<>();
+                        dataToSave.put("title", titleVal);
+                        dataToSave.put("desc", descVal);
+                        dataToSave.put("image", downloadurl.toString());
+                        dataToSave.put("timestamp", String.valueOf(System.currentTimeMillis()));
+                        dataToSave.put("userid", mUser.getUid());
+                        dataToSave.put("username", mUser.getEmail());
+
+                        newPost.setValue(dataToSave);
+                        mProgress.dismiss();
+
+                        startActivity(new Intent(AddPostActivity.this, PostListActivity.class));
+                        finish();
+
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AddPostActivity.this, "Oops! Something went Wrong", Toast.LENGTH_SHORT).show();
+                    mProgress.dismiss();
+                }
+            });
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/jmac/application/worldwide/Data/BlogRecyclerAdapter.java
@@ -0,0 +1,102 @@
+package jmac.application.worldwide.Data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Date;
+import java.util.List;
+
+import jmac.application.worldwide.Model.Blog;
+import jmac.application.worldwide.R;
+
+public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Blog> blogList;
+
+    public BlogRecyclerAdapter(Context context, List<Blog> blogList) {
+        this.context = context;
+        this.blogList = blogList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.post_row, parent, false);
+
+
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Blog blog = blogList.get(position);
+        String imageUrl = null;
+
+        holder.title.setText(blog.getTitle());
+        holder.desc.setText(blog.getDesc());
+
+
+        java.text.DateFormat dateFormat = java.text.DateFormat.getDateInstance();
+        String formattedDate = dateFormat.format(new Date(Long.valueOf(blog.getTimestamp())).getTime());
+
+
+
+        holder.timestamp.setText(formattedDate);
+
+        imageUrl = blog.getImage();
+
+
+        Picasso.with(context)
+                .load(imageUrl)
+                .into(holder.image);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return blogList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView desc;
+        public TextView timestamp;
+        public ImageView image;
+        String userid;
+
+        public ViewHolder(View view, Context ctx) {
+            super(view);
+
+            context = ctx;
+
+            title = (TextView) view.findViewById(R.id.postTitleList);
+            desc = (TextView) view.findViewById(R.id.postTextList);
+            image = (ImageView) view.findViewById(R.id.postImageList);
+            timestamp = (TextView) view.findViewById(R.id.timestampList);
+
+            userid = null;
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // we can go to the next activity...
+
+                }
+            });
+
+        }
+    }
+}
